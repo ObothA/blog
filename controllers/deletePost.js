022a72a4efe0d7a5b71e362d39ffbfb51ca7cdff
obotha <khyper234@gmail.com>
2019-01-16 17:03:22 +0300

    Delete post media on delete of post

--- controllers/deletePost.js
+++ controllers/deletePost.js
@@ -1,10 +1,23 @@
 const Post = require("../database/models/Post");
 const mongoose = require("mongoose");
 const ObjectID = require('mongodb').ObjectID;
+const fs = require('fs')
+const path = require("path");
 
-module.exports = (req,res) => {
+module.exports = async(req,res) => {
     const id = req.params.id;
     const postId = new ObjectID(id);
+
+    let post = await Post.find({_id: postId});
+    const filePath =  path.resolve(__dirname, `../public${post[0].media}`);
+
+    /* delete file */
+    try {
+        fs.unlinkSync(filePath)
+        //file removed
+      } catch(err) {
+        console.error(err)
+    }
 
     Post.deleteOne({ _id: postId }, (err,res) => {
         if(err){
