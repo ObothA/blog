305e39e841788f308517314ca970cd2054d4b139
obotha <khyper234@gmail.com>
2019-01-16 16:47:31 +0300

    Delete advert pictures from file system

--- controllers/deleteAdvert.js
+++ controllers/deleteAdvert.js
@@ -1,10 +1,23 @@
 const Advert = require("../database/models/Advert");
 const mongoose = require("mongoose");
 const ObjectID = require('mongodb').ObjectID;
+const fs = require('fs')
+const path = require("path");
 
-module.exports = (req,res) => {
+module.exports = async(req,res) => {
     const id = req.params.AdvertId;
     const advertId = new ObjectID(id);
+
+    let advert = await Advert.find({_id: advertId});
+    const filePath =  path.resolve(__dirname, `../public${advert[0].media}`);
+
+    /* delete file */
+    try {
+        fs.unlinkSync(filePath)
+        //file removed
+      } catch(err) {
+        console.error(err)
+      }
 
     Advert.deleteOne({ _id: advertId }, (err,res) => {
         if(err){
